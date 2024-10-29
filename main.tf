fb755c3d85b7471982d9470bffbb167b8347fc1f
mapagulayan88 <152959839+mapagulayan88@users.noreply.github.com>
2024-10-30 12:26:02 +1300

    error in vpc_id line 35

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ data "aws_vpc" "default" {
 resource "aws_security_group" "blog" {
   name        = "blog"
   description = "Allow HTTP/S in. Allow everything out."
-  vpc_id      = data_aws_vpc.default.id
+  vpc_id      = data.aws_vpc.default.id
 }
 
 resource "aws_security_group_rule" "blog_http_in" {
