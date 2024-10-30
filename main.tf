7de0b4297ba4bf96b1886d75547daf6d3d9d45d4
mapagulayan88 <152959839+mapagulayan88@users.noreply.github.com>
2024-10-30 13:29:39 +1300

    remove alb

--- main.tf
+++ main.tf
@@ -47,42 +47,7 @@ module "blog_vpc" {
   }
 }
 
-module "blog-alb" {
-  source = "terraform-aws-modules/alb/aws"
 
-  name            = "my-alb"
-  vpc_id          = module.blog_vpc.vpc_id
-  subnets         = module.blog_vpc.public_subnets
-  security_groups = [module.blog_sg.security_group_id]
-
-
-  target_groups = [
-    {
-      name_prefix      = "blog-"
-      backend_protocol = "HTTP"
-      backend_port     = 80
-      target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.web.id
-        }
-      }
-    }
-  ]
-
-  listeners = {
-    ex-tcp = {
-      port                = 80
-      protocol            = "HTTP"
-      target_group_index  = 0
-    }
-  }
-    
-  tags = {
-    Environment = "Development"
-    Project     = "learning terraform"
-  }
-}
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
